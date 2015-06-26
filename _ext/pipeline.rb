a7ceacc66decd8166e7e38bc749fe9474c1f490d
Emmanuel Bernard <emmanuel@hibernate.org>
2015-06-25 22:02:30 -0400

    Add Asciidoc support inlined and coderay rendering

--- _ext/pipeline.rb
+++ _ext/pipeline.rb
@@ -1,5 +1,31 @@
 require 'zurb-foundation'
 
+# dependencies for asciidoc support
+require 'tilt'
+require 'haml'
+require 'asciidoctor'
+
+# hack to add asciidoc support in HAML
+# remove once haml_contrib has accepted the asciidoc registration patch
+# :asciidoc
+#   some block content
+#
+# :asciidoc
+#   :doctype: inline
+#   some inline content
+#
+if !Haml::Filters.constants.map(&:to_s).include?('AsciiDoc')
+  Haml::Filters.register_tilt_filter 'AsciiDoc'
+  Haml::Filters::AsciiDoc.options[:safe] = :safe
+  Haml::Filters::AsciiDoc.options[:attributes] ||= {}
+  Haml::Filters::AsciiDoc.options[:attributes]['notitle!'] = ''
+  # copy attributes from site.yml
+  attributes = site.asciidoctor[:attributes].each do |key, value|
+  Haml::Filters::AsciiDoc.options[:attributes][key] = value
+  end
+end
+
+# Proper pipeline
 Awestruct::Extensions::Pipeline.new do
   # extension Awestruct::Extensions::Posts.new '/news'
   extension Awestruct::Extensions::Indexifier.new
